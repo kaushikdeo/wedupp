026116a1a4375000074388052ff092cb42358bc0
Kaushik <kaushikmdeo@gmail.com>
2019-03-14 07:10:32 +0530

    Added scripts on backend to start both frontend and backend with one script

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [
