dbfb53c94fad3beed26c1a45433d3d2c8f70d58e
Kaushik <kaushikmdeo@gmail.com>
2019-03-14 06:52:50 +0530

    Added babel, webpack and configured webpack-dev-server on frontend

---
+++ client/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HTMLWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'}
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
